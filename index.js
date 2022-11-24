16649ca01d86924119c72f5e37fc6972601f8444
Sayem92 <sayemmiha123@gmail.com>
2022-11-24 12:22:58 +0600

    basic setup

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
+
+const jwt = require('jsonwebtoken')
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middle ware--
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.lhckmem.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+
+        const categoriesCollection = client.db("SellPhones").collection("categories");
+
+      
+
+
+         
+
+
+    }
+    catch (err) {
+        console.log(err);
+    }
+}
+
+
+run().catch(err => console.log(err))
+
+
+
+app.get('/', (req, res) => {
+    res.send('Sell phones BD server is running.............');
+});
+
+app.listen(port, () => {
+    console.log('Sell phones BD server running on:', port);
+})
