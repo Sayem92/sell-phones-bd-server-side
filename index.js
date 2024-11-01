bbdb113d8d639d544883728d842adeacc585db95
Sayem92 <sayemmiha123@gmail.com>
2024-11-02 01:47:55 +0600

    add origin

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ const port = process.env.PORT || 5000;
 const allowedOrigins = [
   "http://localhost:3000",
   "https://yourfrontenddomain.com",
+  'https://sell-phones-bd.web.app'
 ];
 app.use(
   cors({
